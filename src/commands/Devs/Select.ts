b3e523ca7ffb5d7d062f8742c2e77cfb8f65510c
Mystic#9697 <shadow6060@users.noreply.github.com>
2023-03-14 18:14:37 -0600

    2 New commands Simple ones though

---
+++ src/commands/Devs/Select.ts
@@ -0,0 +1,56 @@
+import { development } from "../../providers/env";
+import { execSync } from "child_process";
+import { format } from "../../utils/string";
+import { transpile } from "typescript";
+import { permissions } from "../../providers/permissions";
+import { Command } from "../../structures/Command";
+import { client } from "../../providers/client";
+import { channel } from "diagnostics_channel";
+import { StringSelectMenuBuilder, EmbedBuilder } from "discord.js";
+
+export const command = new Command("select", "Select Menu Example")
+	.addPermission(permissions.developer) // add permission here
+	.setExecutor(async (interaction) => {
+		const select = new StringSelectMenuBuilder()
+			.setCustomId("select")
+			.setPlaceholder("Choose an option")
+			.addOptions([
+				{
+					label: "Option 1",
+					value: "option_1",
+					description: "The first option",
+					type: 3 // changed from "SELECT_MENU" to 3
+				},
+				{
+					label: "Option 2",
+					value: "option_2",
+					description: "The second option",
+					type: 3 // changed from "SELECT_MENU" to 3
+				},
+				{
+					label: "Option 3",
+					value: "option_3",
+					description: "The third option",
+					type: 3 // changed from "SELECT_MENU" to 3
+				},
+			]);
+
+		const embed = new EmbedBuilder()
+			.setColor("#0099ff")
+			.setTitle("Select Menu Example")
+			.setDescription("Please select an option:")
+			.addFields(
+				{ name: "Option 1", value: "The first option" },
+				{ name: "Option 2", value: "The second option" },
+				{ name: "Option 3", value: "The third option" }
+			);
+		await interaction.reply({
+			embeds: [embed],
+			components: [{
+				type: 1, // changed from "ACTION_ROW" to 1
+				components: [select]
+			}],
+		});
+	});
+
+module.exports = { command };
