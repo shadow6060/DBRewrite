052c81b551d31b20420281579c5baff52e2feee1
Mystic#9697 <shadow6060@users.noreply.github.com>
2024-04-19 19:12:17 -0400

    Delete src/structures/DrinksData.ts
    Not needed

--- src/structures/DrinksData.ts
+++
@@ -1,6 +0,0 @@
-export const drinksData = [
-	{ name: "Drink11", type: "normal", price: 10 },
-	{ name: "Drink22", type: "normal", price: 8 },
-	{ name: "Drink33", type: "normal", price: 12 },
-	{ name: "Drink32", type: "normal", price: 13 },
-];
