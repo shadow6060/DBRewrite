3476a56f3bcdbe21e7465e7c99466226d9246cc9
Mystic#9697 <shadow6060@users.noreply.github.com>
2025-04-01 19:43:54 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import "./providers/client";
+import { client } from "./providers/client";
 import { development, production } from "./providers/env";
 import "source-map-support/register";
-// experimental API
 // import "./providers/apiServer";
+
+import { messageCreateHandler } from "./events/MessageCreate";
+
+client.on("messageCreate", messageCreateHandler);
+
 
 if (development) console.warn("Starting in development mode!");
 if (production) console.warn("Starting in production mode!");
-
