1377a867b40e0def519c4f9dace44779419e34bc
Mystic#9697 <shadow6060@users.noreply.github.com>
2025-04-09 02:32:11 +0000

    Delete src/commands/Devs/terms.ts

--- src/commands/Devs/terms.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "../../structures/Command";
-export const command = new Command("tos", "Gives you a link to our tos.")
-	.setExecutor(async int => {
-		int.reply("https://drunk-bartender.org/Terms_of_Service");
-	});
