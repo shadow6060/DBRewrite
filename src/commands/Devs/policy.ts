f3a50e8af66023a76eb89436f5bb5fb93779a421
Mystic#9697 <shadow6060@users.noreply.github.com>
2025-04-09 02:32:03 +0000

    Delete src/commands/Devs/policy.ts

--- src/commands/Devs/policy.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "../../structures/Command";
-export const command = new Command("policy", "Gives you a link to our policy.")
-	.setExecutor(async int => {
-		int.reply("https://drunk-bartender.org/Policy");
-	});
